62d88f36a8965633c52aece36b9e63a06a215175
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 17:12:02 +0000

    libxml2 2.14.4
    libxml2: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libxml2.rb
+++ Formula/lib/libxml2.rb
@@ -1,23 +1,9 @@
 class Libxml2 < Formula
   desc "GNOME XML library"
   homepage "http://xmlsoft.org/"
+  url "https://download.gnome.org/sources/libxml2/2.14/libxml2-2.14.4.tar.xz"
+  sha256 "24175ec30a97cfa86bdf9befb7ccf4613f8f4b2713c5103e0dd0bc9c711a2773"
   license "MIT"
-
-  stable do
-    url "https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.8.tar.xz"
-    sha256 "277294cb33119ab71b2bc81f2f445e9bc9435b893ad15bb2cd2b0e859a0ee84a"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-
-    # Fix pkg-config checks for libicuuc. Patch taken from:
-    # https://gitlab.gnome.org/GNOME/libxml2/-/commit/b57e022d75425ef8b617a1c3153198ee0a941da8
-    # When the patch is no longer needed, remove along with the `stable` block
-    # and the autotools dependencies above. Also uncomment `if build.head?`
-    # condition in the `install` block.
-    patch :DATA
-  end
 
   # We use a common regex because libxml2 doesn't use GNOME's "even-numbered
   # minor is stable" version scheme.
@@ -50,15 +36,9 @@ class Libxml2 < Formula
   depends_on "python-setuptools" => :build
   depends_on "python@3.12" => [:build, :test]
   depends_on "python@3.13" => [:build, :test]
-  depends_on "icu4c@77"
   depends_on "readline"
 
   uses_from_macos "zlib"
-
-  def icu4c
-    deps.find { |dep| dep.name.match?(/^icu4c(@\d+)?$/) }
-        .to_formula
-  end
 
   def pythons
     deps.map(&:to_formula)
@@ -67,37 +47,15 @@ class Libxml2 < Formula
   end
 
   def install
-    # Work around build failure due to icu4c 75+ adding -std=c11 to installed
-    # files when built without manually setting "-std=" in CFLAGS. This causes
-    # issues on Linux for `libxml2` as `addrinfo` needs GNU extensions.
-    # nanohttp.c:1019:42: error: invalid use of undefined type 'struct addrinfo'
-    ENV.append "CFLAGS", "-std=gnu11" if OS.linux?
-
-    system "autoreconf", "--force", "--install", "--verbose" # if build.head?
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
     system "./configure", "--disable-silent-rules",
                           "--sysconfdir=#{etc}",
                           "--with-history",
-                          "--with-http",
-                          "--with-icu",
                           "--with-legacy", # https://gitlab.gnome.org/GNOME/libxml2/-/issues/751#note_2157870
                           "--without-lzma",
                           "--without-python",
                           *std_configure_args
     system "make", "install"
-
-    inreplace [bin/"xml2-config", lib/"pkgconfig/libxml-2.0.pc"] do |s|
-      s.gsub! prefix, opt_prefix
-      s.gsub! icu4c.prefix.realpath, icu4c.opt_prefix, audit_result: false
-    end
-
-    # `icu4c` is keg-only, so we need to tell `pkg-config` where to find its
-    # modules.
-    if OS.mac?
-      icu_uc_pc = icu4c.opt_lib/"pkgconfig/icu-uc.pc"
-      inreplace lib/"pkgconfig/libxml-2.0.pc",
-                /^Requires\.private:(.*)\bicu-uc\b(.*)$/,
-                "Requires.private:\\1#{icu_uc_pc}\\2"
-    end
 
     sdk_include = if OS.mac?
       sdk = MacOS.sdk_path_if_needed
@@ -160,23 +118,3 @@ class Libxml2 < Formula
     end
   end
 end
-
-__END__
-diff --git a/configure.ac b/configure.ac
-index c6dc93d58f84f21c4528753d2ee1bc1d50e67ced..e7bad24d8f1aa7659e1aa4e2ad1986cc2167483b 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -984,10 +984,10 @@ if test "$with_icu" != "no" && test "$with_icu" != "" ; then
-
-     # Try pkg-config first so that static linking works.
-     # If this succeeeds, we ignore the WITH_ICU directory.
--    PKG_CHECK_MODULES([ICU], [icu-i18n], [
--        WITH_ICU=1; XML_PC_REQUIRES="${XML_PC_REQUIRES} icu-i18n"
-+    PKG_CHECK_MODULES([ICU], [icu-uc], [
-+        WITH_ICU=1; XML_PC_REQUIRES="${XML_PC_REQUIRES} icu-uc"
-         m4_ifdef([PKG_CHECK_VAR],
--            [PKG_CHECK_VAR([ICU_DEFS], [icu-i18n], [DEFS])])
-+            [PKG_CHECK_VAR([ICU_DEFS], [icu-uc], [DEFS])])
-         if test "x$ICU_DEFS" != "x"; then
-             ICU_CFLAGS="$ICU_CFLAGS $ICU_DEFS"
-         fi],[:])
