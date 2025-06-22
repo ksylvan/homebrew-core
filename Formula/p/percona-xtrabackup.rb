9f71da869acd2cfd7657308ec0ca2f5cd5de8456
Rui Chen <rui@chenrui.dev>
2025-06-21 20:25:46 -0400

    percona-xtrabackup: revision bump (protobuf 30.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/percona-xtrabackup.rb
+++ Formula/p/percona-xtrabackup.rb
@@ -4,7 +4,7 @@ class PerconaXtrabackup < Formula
   url "https://downloads.percona.com/downloads/Percona-XtraBackup-8.4/Percona-XtraBackup-8.4.0-2/source/tarball/percona-xtrabackup-8.4.0-2.tar.gz"
   sha256 "0777e3d3c3b4d4649ed23ed7197ec0aa71379b4a4a41b969b7286f6cf8888b4a"
   license "GPL-2.0-only"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://www.percona.com/products-api.php", post_form: {
@@ -69,6 +69,9 @@ class PerconaXtrabackup < Formula
   patch :DATA
 
   def install
+    # Support cmake 4
+    ENV["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
+
     # Remove bundled libraries other than explicitly allowed below.
     # `boost` and `rapidjson` must use bundled copy due to patches.
     # `lz4` is still needed due to xxhash.c used by mysqlgcs
